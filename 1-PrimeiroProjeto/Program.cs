ab97a0950500b63dc67d8fdb28d1677e6a52afb9
ander <ander@DESKTOP-TL9PTF4>
2021-09-04 11:11:23 -0300

    Adicionar arquivos de projeto.

---
+++ 1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! Meu primeiro código em CSharp");
+            Console.WriteLine("Fim da execução, tecle enter para fechar ...");
+            Console.ReadLine();
+        }
+    }
+}
