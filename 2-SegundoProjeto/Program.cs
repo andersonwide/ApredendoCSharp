0078e513fe2a19153816311fdca16fc2085bdb70
andersonwide <michaelmags42@outlook.com>
2021-09-04 11:52:17 -0300

    Declarar variável e exibição na tela do console

---
+++ 2-SegundoProjeto/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _2_SegundoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Segundo passo, declarar variáveis do tipo inteira.");
+            int idade = 29;
+            Console.WriteLine(idade);
+            Console.WriteLine("Está é minha idade: " + idade);
+            Console.WriteLine("Está será minha idade daqui a 5 anos: " + (idade + 5));
+
+        }
+    }
+}
